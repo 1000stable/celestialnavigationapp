83128aca467f605e0943b57eff4b82b7caa9a8c9
1000stable <1000stable@gmail.com>
2023-05-21 08:50:54 +1200

    changed URLS back and changes to serve static files

--- celestialnavigationapp/urls.py
+++ celestialnavigationapp/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from celestialnavigationapp import settings
-
-from celestialnavigationapp.settings import STATIC_ROOT
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', include('data_entry.urls')),
     path('admin/', admin.site.urls),
     path('', include('plot.urls')),
-]+ STATIC_ROOT(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
