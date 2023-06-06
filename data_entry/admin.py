5127f356edfa3b8bc3108b62ca1e21d3a96cd9b5
1000stable <1000stable@gmail.com>
2023-06-07 01:01:00 +1200

    Added model to Admin

--- data_entry/admin.py
+++ data_entry/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from data_entry.models import Meridian_Passage_Entry, Meridian_Passage_Sight, SunriseSunsetEntry, SightEntry
+from data_entry.models import Meridian_Passage_Entry, Meridian_Passage_Sight, SunriseSunsetEntry, SightEntry, SightAlmanacEntry
 
 # Register your models here.
 admin.site.register(Meridian_Passage_Entry)
 admin.site.register(Meridian_Passage_Sight)
 admin.site.register(SunriseSunsetEntry)
 admin.site.register(SightEntry)
+admin.site.register(SightAlmanacEntry)
